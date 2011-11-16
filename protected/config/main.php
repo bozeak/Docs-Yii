b4c6cfc2cb54bc28f3862d4d1fc551a5b0db39b1
Alex <bozeak@gmail.com>
2011-11-16 23:19:01 +0200

    Inceracari de a schimba modul de afisare a primii pagini.

--- protected/config/main.php
+++ protected/config/main.php
@@ -49,10 +49,11 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
+                 */
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
--- protected/views/tDb/index.php
+++ protected/views/tDb/index.php
@@ -12,58 +12,74 @@ $this->menu=array(
 
 <h1>Registrul documentelor</h1>
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'tdb-grid',
-        'dataProvider'=>$dataProvider,
-        'enableSorting'=>false,
-	'columns'=>array(
-		array('name'=>'id','value'=>'$data->id','htmlOptions'=>array('style'=>'width:30px',),),
-                array(
-                   // 'name'=>$data[nr_reg]." ".$data[date_reg],
-                    'name'=>'Nr şi data inregistrarii',
-                    'value'=>'nl2br($data->nr_reg)."<br>".nl2br($data->date_reg)',
-                    'type'=>'raw',
-                    'htmlOptions'=>array(
-                        'style'=>'width: 120px',
-                    ),
-                ),
-
-                array('name'=>'elab','value'=>'$data->elab','htmlOptions'=>array('style'=>'width:120px',),),
+<?php //$this->widget('zii.widgets.grid.CGridView', array(
+//	'id'=>'tdb-grid',
+//        'dataProvider'=>$dataProvider,
+//        'enableSorting'=>false,
+//	'columns'=>array(
+//		array('name'=>'id','value'=>'$data->id','htmlOptions'=>array('style'=>'width:30px',),),
 //                array(
-//                    'name'=>'respons_type',
-//                    'value'=>'$data->tipraspuns->name',
+//                   // 'name'=>$data[nr_reg]." ".$data[date_reg],
+//                    'header'=>'Nr și data înregistrării documentului',
+//                    'value'=>'nl2br($data->nr_reg)."<br>".nl2br($data->date_reg)',
 //                    'type'=>'raw',
 //                    'htmlOptions'=>array(
 //                        'style'=>'width: 120px',
 //                    ),
 //                ),
-                array(
-                    'name'=>'responsabil',
-                    'value'=>'nl2br($data->resp->grad0->md)." ".nl2br($data->resp->fullname)."<br>".nl2br($data->resp->contacts)',
-                    'type'=>'raw',
-                    'htmlOptions'=>array(
-                        'style'=>'width: 140px',
-                    ),
-                ),
-		/*
-		'id_elab',
-		'address',
-		'nr_cadastr',
-		'tel',
-		'content',
-		'responsabil',
-		'get_exec',
-		'nr_respons',
-		'date_respons',
-		'respons_type',
-		'note',
-		'dossier',
-		'author',
-		*/
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
-));?>
+//
+//                array('name'=>'elab','value'=>'$data->elab','htmlOptions'=>array('style'=>'width:120px',),),
+////                array(
+////                    'name'=>'respons_type',
+////                    'value'=>'$data->tipraspuns->name',
+////                    'type'=>'raw',
+////                    'htmlOptions'=>array(
+////                        'style'=>'width: 120px',
+////                    ),
+////                ),
+//                array(
+//                    'name'=>'responsabil',
+//                    'value'=>'nl2br($data->resp->grad0->md)." ".nl2br($data->resp->fullname)."<br>".nl2br($data->resp->contacts)',
+//                    'type'=>'raw',
+//                    'htmlOptions'=>array(
+//                        'style'=>'width: 140px',
+//                    ),
+//                ),
+//		/*
+//		'id_elab',
+//		'address',
+//		'nr_cadastr',
+//		'tel',
+//		'content',
+//		'responsabil',
+//		'get_exec',
+//		'nr_respons',
+//		'date_respons',
+//		'respons_type',
+//		'note',
+//		'dossier',
+//		'author',
+//		*/
+//		array(
+//			'class'=>'CButtonColumn',
+//                        'template'=>'{view}{update}',
+//		),
+//	),
+//));?>
 
-
+<?php $this->widget('CLinkPager', array(
+    'pages' => $pages,
+)) ?>
+<table>
+<tr>
+    <td>Id</td>
+</tr>
+<?php foreach($models as $item): ?>
+<tr>
+    <td><?php echo $item->id ?></td>
+</tr>
+<?php endforeach; ?>
+</table>
+<?php $this->widget('CLinkPager', array(
+    'pages' => $pages,
+)) ?>
